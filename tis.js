51c0a903dd1b7f4373cbff545118f78a6b852037
Joppe Derks <joppe.derks@gmail.com>
2024-11-18 16:18:40 +0100

    TIS scroll fix

--- tis.js
+++ tis.js
@@ -1,37 +1,39 @@
 /** @format */
 document.addEventListener("DOMContentLoaded", function () {
-window.scrollBy(0, 100);
-document.body.style.overflow = "hidden";
+    setTimeout(() => {
+        window.scrollBy(0, 100);
+    });
+    document.body.style.overflow = "hidden";
 
-const iCiframe = document.createElement("iframe");
-iCiframe.src =
-    "https://web.iconfigure.nl/?product=95ba6ceb-4779-4424-9104-59fbe594d5d8&type=type_taats&aantal_deuren=1_deur&plaatsing_panelen=paneel_beide&hoogte_sparing=201&breedte_sparing=260&verdeling_panelen=verdeling_gelijk&vlakverdeling=1v&frame_profiel=f_40mm&deur_profiel=40mm&kleur_glas=glas_helder&kleur_staal=staal_9005&structuur=structuur_structuur&handgreep=hondla&active_step=0";
-iCiframe.style.width = "100vw";
-iCiframe.style.left = "0";
-iCiframe.style.position = "fixed";
-iCiframe.style.border = "none";
-iCiframe.style.zIndex = "9999";
+    const iCiframe = document.createElement("iframe");
+    iCiframe.src =
+        "https://web.iconfigure.nl/?product=95ba6ceb-4779-4424-9104-59fbe594d5d8&type=type_taats&aantal_deuren=1_deur&plaatsing_panelen=paneel_beide&hoogte_sparing=201&breedte_sparing=260&verdeling_panelen=verdeling_gelijk&vlakverdeling=1v&frame_profiel=f_40mm&deur_profiel=40mm&kleur_glas=glas_helder&kleur_staal=staal_9005&structuur=structuur_structuur&handgreep=hondla&active_step=0";
+    iCiframe.style.width = "100vw";
+    iCiframe.style.left = "0";
+    iCiframe.style.position = "fixed";
+    iCiframe.style.border = "none";
+    iCiframe.style.zIndex = "9999";
 
-if (window.innerWidth <= 768) {
-    iCiframe.style.top = "90px";
-    iCiframe.style.height = "100DVH";
-    iCiframe.style.bottom = "110px";
-} else {
-    iCiframe.style.top = "80px";
-    iCiframe.style.height = "100DVH";
-    iCiframe.style.bottom = "0px";
-}
-console.log(document.body);
-document.body.appendChild(iCiframe);
+    if (window.innerWidth <= 768) {
+        iCiframe.style.top = "90px";
+        iCiframe.style.height = "100DVH";
+        iCiframe.style.bottom = "110px";
+    } else {
+        iCiframe.style.top = "80px";
+        iCiframe.style.height = "100DVH";
+        iCiframe.style.bottom = "0px";
+    }
+    console.log(document.body);
+    document.body.appendChild(iCiframe);
 
-setTimeout(() => {
-    var item = document.querySelector('iframe[title="Weply chat"]');
-    console.log(item);
-    item.remove();
-    item = document.querySelector('iframe[title="reCAPTCHA"]');
-    console.log(item);
-    item.remove();
-}, 100);
+    setTimeout(() => {
+        var item = document.querySelector('iframe[title="Weply chat"]');
+        console.log(item);
+        item.remove();
+        item = document.querySelector('iframe[title="reCAPTCHA"]');
+        console.log(item);
+        item.remove();
+    }, 100);
 });
 function redirect(to) {
     const icLink = document.createElement("a");
@@ -47,4 +49,4 @@ window.addEventListener("message", (event) => {
         redirect("/stalen-deuren-showroom");
     }
 });
-// https://thuisinstaal.nl/stalen-deuren-showroom/ 
+// https://thuisinstaal.nl/stalen-deuren-showroom/
