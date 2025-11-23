aade13de2b10b8895babe9ed8034770ca141ccde
Joppe44 <joppe.derks@gmail.com>
2025-11-23 10:43:16 +0100

    changed the ifee kastenland v2

--- kastenland.js
+++ kastenland.js
@@ -1,23 +1,45 @@
 /** @format */
+function initIConfigure() {
+    console.log("iConfigure loaded from scripts.iconfigure.nl");
+    var baseUrl = "https://web.iconfigure.nl/?product=3139b1fc-842f-4742-9130-17272cc60fd3";
 
-console.log("iConfigure loaded from scripts.iconfigure.nl");
-var baseUrl = "https://web.iconfigure.nl/?product=3139b1fc-842f-4742-9130-17272cc60fd3";
+    document.querySelector("footer").remove();
+    const holder = document.getElementById("MainContent");
 
-document.querySelector("footer").remove();
-const holder = document.getElementById("MainContent");
+    let toparse = ["hoogte", "breedte", "diepte"];
 
-let toparse = ["hoogte", "breedte", "diepte"];
-
-function getIframeSRC() {
-    let params = new URLSearchParams(document.location.search);
-    for (const p of toparse) {
-        let val = params.get(p);
-        if (val) baseUrl = baseUrl + "&" + p + "=" + val;
+    function getIframeSRC() {
+        let params = new URLSearchParams(document.location.search);
+        for (const p of toparse) {
+            let val = params.get(p);
+            if (val) baseUrl = baseUrl + "&" + p + "=" + val;
+        }
+        console.log(baseUrl);
+        return baseUrl;
     }
-    console.log(baseUrl);
+
+    const iframe = document.createElement("iframe");
+
+    iframe.src = getIframeSRC();
+    holder.appendChild(iframe);
+    recalculateIframe();
+    function recalculateIframe() {
+        let rect = document.querySelector(".header-wrapper").getBoundingClientRect();
+        const height = rect.top + rect.height;
+        iframe.style.position = "fixed";
+        iframe.style.height = `calc(100dvh - ${height}px)`;
+        iframe.style.top = height + "px";
+        iframe.style.width = "100vw";
+    }
+    window.addEventListener("resize", () => {
+        recalculateIframe();
+    });
+    iframe.style.border = "none";
 }
 
-const iframe = document.createElement("iframe");
-
-iframe.src = getIframeSRC();
-holder.appendChild(iframe);
+// Run after base page load (all resources)
+if (document.readyState === "complete") {
+    initIConfigure();
+} else {
+    window.addEventListener("load", initIConfigure);
+}
