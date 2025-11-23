3eea9202205a1f19e4f42d0b94a817cadf7868b2
Joppe44 <joppe.derks@gmail.com>
2025-11-23 10:40:09 +0100

    added kastenland v1

--- kastenland.js
+++ kastenland.js
@@ -1,1 +1,23 @@
-console.log("iConfigure loaded from scripts.iconfigure.nl")
+/** @format */
+
+console.log("iConfigure loaded from scripts.iconfigure.nl");
+var baseUrl = "https://web.iconfigure.nl/?product=3139b1fc-842f-4742-9130-17272cc60fd3";
+
+document.querySelector("footer").remove();
+const holder = document.getElementById("MainContent");
+
+let toparse = ["hoogte", "breedte", "diepte"];
+
+function getIframeSRC() {
+    let params = new URLSearchParams(document.location.search);
+    for (const p of toparse) {
+        let val = params.get(p);
+        if (val) baseUrl = baseUrl + "&" + p + "=" + val;
+    }
+    console.log(baseUrl);
+}
+
+const iframe = document.createElement("iframe");
+
+iframe.src = getIframeSRC();
+holder.appendChild(iframe);
