f463ba8f2a02a96ff53daea2bf45a8095b9f003b
Joppe44 <joppe.derks@gmail.com>
2025-11-23 10:58:29 +0100

    fix kastenland 2

--- kastenland.js
+++ kastenland.js
@@ -1,42 +1,42 @@
 /** @format */
 
-var mainContent = document.getElementById("MainContent");
-var div = document.createElement("div");
-div.id = "iConfigure";
-div.style.position = "sticky";
-div.style.height = "calc(100dvh)";
-div.style.width = "100vw";
-div.style.zIndex = "1023";
-div.style.pointerEvents = "auto";
-div.style.top = "0";
-mainContent.appendChild(div);
-
-var link = document.createElement("link");
-link.rel = "stylesheet";
-link.media = "all";
-link.href = "https://web.iconfigure.nl/inject/style.css";
-document.head.appendChild(link);
-
-var script = document.createElement("script");
-script.src = "https://web.iconfigure.nl/inject/inject.iife.js";
-script.crossOrigin = "anonymous";
-script.onload = function () {
-    let preConfig = {
-        product: "3139b1fc-842f-4742-9130-17272cc60fd3",
-    };
-
-    let toparse = ["hoogte", "breedte", "diepte"];
-    let params = new URLSearchParams(document.location.search);
-    for (const p of toparse) {
-        let val = params.get(p);
-        if (val) preConfig[p] = val;
-    }
-
-    injectApp(preConfig);
-};
-
 window.addEventListener("DOMContentLoaded", function () {
     document.querySelector("footer").remove();
-});
 
-document.head.appendChild(script);
+    var mainContent = document.getElementById("MainContent");
+    var div = document.createElement("div");
+    div.id = "iConfigure";
+    div.style.position = "sticky";
+    div.style.height = "calc(100dvh)";
+    div.style.width = "100vw";
+    div.style.zIndex = "1023";
+    div.style.pointerEvents = "auto";
+    div.style.top = "0";
+    mainContent.appendChild(div);
+
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.media = "all";
+    link.href = "https://web.iconfigure.nl/inject/style.css";
+    document.head.appendChild(link);
+
+    var script = document.createElement("script");
+    script.src = "https://web.iconfigure.nl/inject/inject.iife.js";
+    script.crossOrigin = "anonymous";
+    script.onload = function () {
+        let preConfig = {
+            product: "3139b1fc-842f-4742-9130-17272cc60fd3",
+        };
+
+        let toparse = ["hoogte", "breedte", "diepte"];
+        let params = new URLSearchParams(document.location.search);
+        for (const p of toparse) {
+            let val = params.get(p);
+            if (val) preConfig[p] = val;
+        }
+
+        injectApp(preConfig);
+    };
+
+    document.head.appendChild(script);
+});
