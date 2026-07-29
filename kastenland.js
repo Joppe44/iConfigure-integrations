4164e143969e464110e5ab792299bfac57b05b2e
Joppe44 <joppe.derks@gmail.com>
2026-07-29 19:05:21 +0200

    kastenland: forward all page url params to iframe src

--- kastenland.js
+++ kastenland.js
@@ -65,11 +65,9 @@
 
     var src = new URL("https://configurator.iconfigure.dev/");
     src.searchParams.set("product", "c9018a98-f48b-407f-bd38-732b5acc0adc");
-    var toparse = ["breedte", "lengte", "hoogte", "diepte"];
     var params = new URLSearchParams(document.location.search);
-    for (const p of toparse) {
-      var val = params.get(p);
-      if (val) src.searchParams.set(p, val);
+    for (const [key, val] of params) {
+      if (val && key !== "product") src.searchParams.set(key, val);
     }
 
     var iframe = document.createElement("iframe");
